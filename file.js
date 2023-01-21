ed395a19d666bf076efea8beb5d345c8f124ab34
Omkar141998 <onagpure688@gmail.com>
2023-01-22 02:37:08 +0530

    ohkk

---
+++ file.js
@@ -0,0 +1,25 @@
+//to do make m+, M- and mc Funtional
+
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+       if(e.target.innerHTML == '='){
+        string = eval(string);
+        document.querySelector('input').value = string;
+       }
+       else if(e.target.innerHTML == 'AC'){
+        string = "";
+        document.querySelector('input').value = string;
+       }
+       else if(e.target.innerHTML == 'x'){
+        string = "10*3";
+        document.querySelector('input').value = string;
+       }
+       else{
+        console.log(e.target)
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+    }
+    })
+})
